f275ab3d8ed98781f338221b25ac7ed67d7da3ae
Plamen Dobrev <applamen@me.com>
2024-01-19 11:01:06 +0200

    remove custom json parser

--- api-gateway/src/main/java/com/myfinances/apigateway/services/AuthUserService.java
+++ api-gateway/src/main/java/com/myfinances/apigateway/services/AuthUserService.java
@@ -1,7 +1,5 @@
 package com.myfinances.apigateway.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myfinances.apigateway.model.entities.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -18,19 +16,11 @@ public class AuthUserService {
     }
 
     public Optional<User> findUserByUserName(String userName) {
-        String result = restClient.get()
+        User result = restClient.get()
                 .uri("/findUserByUserName/" + userName)
                 .retrieve()
-                .body(String.class);
+                .body(User.class);
 
-        ObjectMapper mapper = new ObjectMapper();
-        User user;
-        try {
-            user = mapper.readValue(result, User.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-        return user != null ? Optional.of(user) : Optional.empty();
+        return result != null ? Optional.of(result) : Optional.empty();
     }
 }
