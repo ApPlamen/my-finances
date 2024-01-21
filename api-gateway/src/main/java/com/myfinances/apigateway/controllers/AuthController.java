bed22fef605593f85fae00853b494d117e41235b
Plamen Dobrev <applamen@me.com>
2024-01-21 11:05:53 +0200

    add error handling

--- api-gateway/src/main/java/com/myfinances/apigateway/controllers/AuthController.java
+++ api-gateway/src/main/java/com/myfinances/apigateway/controllers/AuthController.java
@@ -1,13 +1,10 @@
 package com.myfinances.apigateway.controllers;
 
 import com.myfinances.apigateway.model.request.LoginReq;
-import com.myfinances.apigateway.model.response.ErrorRes;
 import com.myfinances.apigateway.model.response.LoginRes;
 import com.myfinances.apigateway.services.AuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,16 +21,8 @@ public class AuthController {
     }
 
     @PostMapping(value = "/login")
-    public ResponseEntity login(@RequestBody LoginReq loginReq) {
-        try {
-            LoginRes loginRes = authService.login(loginReq);
-            return ResponseEntity.ok(loginRes);
-        } catch (BadCredentialsException e) {
-            ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, "Invalid username or password");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-        } catch (Exception e) {
-            ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-        }
+    public ResponseEntity<LoginRes> login(@RequestBody LoginReq loginReq) {
+        LoginRes loginRes = authService.login(loginReq);
+        return ResponseEntity.ok(loginRes);
     }
 }
