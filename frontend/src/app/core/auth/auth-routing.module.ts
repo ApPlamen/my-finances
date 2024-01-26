4e96db922a05a3cd481868bcd4e0fa98770dfad1
Plamen Dobrev <applamen@me.com>
2024-01-26 12:56:26 +0200

    add auth guard

--- frontend/src/app/core/auth/auth-routing.module.ts
+++ frontend/src/app/core/auth/auth-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { AuthGuard } from 'src/app/shared/guards/auth-guard';
 // import { BoardUsersComponent } from './components/board-users/board-users.component';
 // import { ChangePasswordComponent } from './components/change-password/change-password.component';
 import { LogInComponent } from './components/login/login.component';
 import { LogOutComponent } from './components/logout/logout.component';
 // import { ProfileComponent } from './components/profile/profile.component';
 // import { RegisterComponent } from './components/register/register.component';
-// import { RolesEnum } from './enums/roles.enum';
+import { RolesEnum } from './enums/roles.enum';
+import { canActivate } from 'src/app/shared/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LogInComponent },
   // { path: 'register', component: RegisterComponent },
   {
     path: 'logout',
-    // canActivate: [AuthGuard],
-    // data: { accessRoles: [RolesEnum.admin, RolesEnum.companyOwner, RolesEnum.restaurantAdmin, RolesEnum.restaurant, RolesEnum.client] },
+    canActivate: [canActivate],
+    data: { accessRoles: [RolesEnum.user] },
     component: LogOutComponent
   },
   // {
