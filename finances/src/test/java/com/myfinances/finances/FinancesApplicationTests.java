7e988b4d975ddaeb5bf296bdc2a8b64f0ef2b336
Plamen Dobrev <applamen@me.com>
2024-02-05 14:00:14 +0200

    add base infrastructure

---
+++ finances/src/test/java/com/myfinances/finances/FinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfinances.finances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
