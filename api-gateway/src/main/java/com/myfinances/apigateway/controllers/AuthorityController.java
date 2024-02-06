e9811672d2de51dbb4885f38e8ec632372345438
Plamen Dobrev <applamen@me.com>
2024-02-06 09:55:09 +0200

    Controllers config fixes

--- api-gateway/src/main/java/com/myfinances/apigateway/controllers/AuthorityController.java
+++ api-gateway/src/main/java/com/myfinances/apigateway/controllers/AuthorityController.java
@@ -1,7 +1,10 @@
 package com.myfinances.apigateway.controllers;
 
+import com.myfinances.apigateway.configs.OpenAPI30Config;
 import com.myfinances.apigateway.models.response.users.RolesOptionResponse;
 import com.myfinances.apigateway.services.AuthorityService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,8 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/authorities")
+@Tag(name = "Authority Controller")
+@SecurityRequirement(name = OpenAPI30Config.securitySchemeName)
 public class AuthorityController {
     private AuthorityService service;
 
