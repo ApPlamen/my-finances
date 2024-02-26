e7c4af16d2c671e3287e5d1ebf22447062f0cc2a
Plamen Dobrev <applamen@me.com>
2024-02-26 17:15:02 +0200

    add date filter in BE
    add date filter for ChangeByDateStatistic in BE

--- statistics/src/main/java/com/myfinances/statistics/infrastructure/PaymentsRepo.java
+++ statistics/src/main/java/com/myfinances/statistics/infrastructure/PaymentsRepo.java
@@ -2,11 +2,14 @@ package com.myfinances.statistics.infrastructure;
 
 import com.myfinances.statistics.models.KeyValuePair;
 import com.myfinances.statistics.models.request.ChangeByDateStatisticRequest;
+import com.myfinances.statistics.utils.DateUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -15,17 +18,42 @@ public class PaymentsRepo {
     private EntityManager entityManager;
 
     public List<KeyValuePair> getChangeByDate(ChangeByDateStatisticRequest request) {
-        String sql = """
+//        int year = 2024 - 1900;
+//        request.setStartDate(new Date(year, Calendar.FEBRUARY, 02));
+//        request.setEndDate(new Date(year, Calendar.FEBRUARY, 04));
+
+        StringBuilder sql = new StringBuilder("""
                 SELECT sum AS value, to_char(date, 'DD.MM.YYYY') AS name
                 FROM (
                     SELECT SUM(amount) AS sum, cast(date_time AS date) AS date
                     FROM payments
                     WHERE user_id = :userId
-                    GROUP BY date
-                    ORDER BY date);""";
+                    AND active = TRUE
+                """);
 
-        Query query = entityManager.createNativeQuery(sql, "ChangeByDateStatisticResult");
+        if (request.getStartDate() != null) {
+            sql.append("AND date_time >= :startDate").append("\n");
+        }
+        if (request.getEndDate() != null) {
+            sql.append("AND date_time <= :endDate").append("\n");
+        }
+
+        sql.append("""
+                    GROUP BY date
+                    ORDER BY date);
+                """);
+
+        Query query = entityManager.createNativeQuery(sql.toString(), "ChangeByDateStatisticResult");
+
         query.setParameter("userId", request.getUserId());
+        if (request.getStartDate() != null) {
+            query.setParameter("startDate", request.getStartDate());
+        }
+        if (request.getEndDate() != null) {
+            Date endDate = DateUtil.addDays(request.getEndDate(),1);
+            query.setParameter("endDate", endDate);
+        }
+
         var result = query.getResultList();
 
         return result;
