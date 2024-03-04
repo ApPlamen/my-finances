a1e6aaff62fab570849aead69ee10f2dc65acf24
Plamen Dobrev <applamen@me.com>
2024-03-04 21:56:58 +0200

    add fake upload logic

--- frontend/src/app/file-upload/components/file-upload-console/file-upload-console.component.ts
+++ frontend/src/app/file-upload/components/file-upload-console/file-upload-console.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   templateUrl: './file-upload-console.component.html',
 })
 export class FileUploadConsoleComponent {
+  buttonLabel: string = "upload";
+  isDisabled : boolean = false;
+
+  constructor(private toastr: ToastrService) { }
+
+
+  upload(): void {
+    this.buttonLabel = "uploading";
+    this.isDisabled = true;
+    setTimeout(() => this.uploadSuccess(), 2 * 1000);
+  }
+
+  uploadSuccess() {
+    this.toastr.success('Success!');
+    this.buttonLabel = "upload";
+    this.isDisabled = false;
+  }
 }
